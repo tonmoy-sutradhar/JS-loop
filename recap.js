08a3ab02cc9a4c4807df8ade5a77689d1a1f8bcc
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-09-01 20:47:52 +0600

    Loop file added.

---
+++ recap.js
@@ -0,0 +1,19 @@
+// 'const' use korle value change kora jabe nah.
+// 'let' use korle value change kora jabe.
+// var use korbo nah
+
+// variable----->
+const amount = 1000;
+
+// Condition------>
+// Amount ta jodi 2000 tk er kom hoi tahole picnic e jabo---->
+if (amount < 2000) {
+  console.log("Picnic e jabo.");
+} else {
+  console.log("Picnic e jabo nah durrrrrr.");
+}
+
+// Array
+const numbers = [2, 3, 4, 5, 5, 12, 20, 30];
+console.log(numbers);
+console.log(numbers[2]);
